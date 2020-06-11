0c88671682c06e9c105edfa4434dfbce1400d78c
ocampoaleja <omaira.527@hotmail.com>
2020-06-11 17:19:25 -0500

    feature: added list data

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+const list = document.querySelector("#list");
 let x = 10;
-document.write('1. proceso iniciado...');
+//document.write('1. proceso iniciado...');
 
-setTimeout(() => {
+//funcion asincrona
+/*setTimeout(() => {
     x = x * 3 + 2;
     document.write('2.proceso terminado...')
 },5000);
 
-document.write( '3.el resultado es ' +  x )
+document.write( '3.el resultado es ' +  x )*/
+
+//convertirlo a promesas
+const promesa = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        x = x * 3 + 2;
+        list.innerHTML += `<li>2.Proceso terminado...</li>`
+        resolve(x);
+    },5000);
+
+})
+
+list.innerHTML += `<li>1.Proceso iniciado...</li>`;
+
+promesa.then(res => {
+    list.innerHTML += `<li>3.El resultado es: ${res}</li>`
+})
+
