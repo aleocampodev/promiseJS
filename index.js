ad94982a65a2b4bd01bcebab7073073ef19b2c78
ocampoaleja <omaira.527@hotmail.com>
2020-06-11 19:30:04 -0500

    feature: nested promises

--- index.js
+++ index.js
@@ -26,3 +26,54 @@ promesa.then(res => {
     list.innerHTML += `<li>3.El resultado es: ${res}</li>`
 })
 
+//Promesas con array de objetos
+let usuarios = [{
+    id:1,
+    nombre: 'Marcos'
+},
+{
+    id:2,
+    nombre:'Lena'
+}];
+
+let telefonos = [{
+    id:1,
+    telefono: 12345678
+},{
+    id:2,
+    telefono: 87654321
+}];
+
+const obtenerUsuario = id => {
+    //Para obtener el usuario
+    return new Promise((resolve,reject)=>{
+        if(usuarios.find(usuario => usuario.id === id)){
+             document.write('El usuario existe');
+             resolve(obtenerTelefono(2))
+        }else {
+            reject('El usuario no existe')
+        }
+    })
+};
+
+const obtenerTelefono = id => {
+    return new Promise((resolve,reject)=>{
+        if(telefonos.find( telefono => telefono.id === id)){
+            resolve(`<div>El telefono existe</div>`)
+        }else {
+            reject(`<div>El telefono no existe</div>`)
+        }
+    })
+};
+
+obtenerUsuario(1)
+.then(res => {
+    return res 
+})
+.then(mensaje => {
+    document.write(mensaje)
+})
+.catch(error => {
+    console.error(error)
+});
+
